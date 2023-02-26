070e97e794636b9d2aabb9e27882e72ffebd2166
mora <mora@omen.mora>
2023-02-26 17:08:29 +0100

    added settings.py

--- python_crash/Proyectos/alien_invasion/alien_invasion.py
+++ python_crash/Proyectos/alien_invasion/alien_invasion.py
@@ -1,6 +1,6 @@
 import sys
-
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """Clase general para gestionar los recursos y el comportamiento del juego."""
@@ -8,9 +8,12 @@ class AlienInvasion:
     def __init__(self):
         """Inicializa el juego y sus recursos."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")       
+
     
     def run_game(self):
         """Inicializa el bucle principal para el juego."""
@@ -19,10 +22,15 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            #accedemos al color de la pantalla de fond, para redibujar la pantalla en cada paso por el bucle
+            self.screen.fill(self.bg_color)        
+            
             pygame.display.flip()
-    
+
 
 if __name__ == '__main__':
     # Hace una instancia del juego y lo ejecuta.
     ai = AlienInvasion()
     ai.run_game()
+
