402eab4409527301bc7cf75cc9050727b5a6ff76
jaimmart32 <vivalafruta11@gmail.com>
2023-02-26 18:33:16 +0100

    ship movement settings

--- python_crash/Proyectos/alien_invasion/settings.py
+++ python_crash/Proyectos/alien_invasion/settings.py
@@ -5,9 +5,12 @@ class Settings:
     def __init__(self):
         '''Inicializa la configuaración dle juego'''
 
-        #Configuraciń de la pantalla
+        # Configuraciń de la pantalla
         self.screen_width  = 1200
         self.screen_height = 800
-        self.bg.color = (230, 230, 230)
+        self.bg_color = (230, 230, 230)
+
+        # Configuarción de la nave.
+        self.ship_speed = 1.5
 
         
---
+++ python_crash/Proyectos/alien_invasion/ship.py
@@ -0,0 +1,42 @@
+import pygame
+
+class Ship:
+    """Una clase para gestionar la nave."""
+    def __init__(self, ai_game):
+        """Inicializa la nave y configura su posición inicial."""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.settings = ai_game.settings
+
+        # Carga la imagen de la nave y obtiene su rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Coloca inicialmente cada nave nueva en el centro inferior de la pantalla.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Guarda un valor decimal para la poscición horizontal de la nave
+        self.x = float(self.rect.x)
+
+        #Bandera de movimiento.
+        self.moving_right = False
+        self.moving_left = False
+    
+    def update(self):
+        """Actualiza la posición de la nave en funcion de labandera de movimiento"""
+        # Actualiza el valor x de la nave, no el rect.
+        if self.moving_right:
+            self.rect.x += settings.ship_speed
+
+        if self.moving_left:
+            self.rect.x -= settings.ship_speed
+
+        # Actualiza el objeto rect de self.x
+        self.rect.x = self.x
+        
+    
+    def blitme(self):
+        """Dibuja la nave en su ubicación actual."""
+        self.screen.blit(self.image, self.rect)
+        
