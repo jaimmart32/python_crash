87c6e3303d6db115336c72f4452b694bd45471ee
jaimmart32 <vivalafruta11@gmail.com>
2023-03-04 20:38:28 +0100

    bullets

--- python_crash/Proyectos/alien_invasion/alien_invasion.py
+++ python_crash/Proyectos/alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Clase general para gestionar los recursos y el comportamiento del juego."""
@@ -19,6 +20,7 @@ class AlienInvasion:
 
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     
     def run_game(self):
@@ -27,6 +29,10 @@ class AlienInvasion:
             # Busca eventos de teclado y ratón
             self._check_events()
             self.ship.update()
+            self._update_bullets()
+
+            
+
             # Accedemos al color de la pantalla de fond, para redibujar la pantalla en cada paso por el bucle
             self._update_screen()
             
@@ -52,6 +58,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
     
     def _check_keyup_events(self, event):
             
@@ -60,16 +68,32 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
             self.ship.moving_left = False
                         
-                        
+    def _fire_bullet(self):
+        """Crea una bala nueva y la añade al grupo de balas"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)                   
 
     
     def _update_screen(self):
         """Actualiza las imagenes en la pantalla y cambia a la pantalla nueva."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-            
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet() 
         pygame.display.flip()
     
+    def _update_bullets(self):
+        """Actualioza la posición de las balas y de deshace de las viejas"""
+        # Actualiza la posiución de las balas
+        self.bullets.update()
+
+        # Se deshace de las balas que han desaparecido
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+            
+
 
 
 if __name__ == '__main__':
--- python_crash/Proyectos/alien_invasion/settings.py
+++ python_crash/Proyectos/alien_invasion/settings.py
@@ -13,4 +13,14 @@ class Settings:
         # Configuarción de la nave.
         self.ship_speed = 1.5
 
+        #Configuración de las balas
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 7
+
+
+
+
         
